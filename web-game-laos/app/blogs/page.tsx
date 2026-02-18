64d54fd688b4b535986c4883712fb25978935c26
RachaponKjr <rachapon.navy@gmail.com>
2026-02-18 17:35:45 +0700

    fix

--- web-game-laos/app/blogs/page.tsx
+++ web-game-laos/app/blogs/page.tsx
@@ -2,7 +2,9 @@ import { Blog } from "./[id]/_components/blog-page";
 import BlogsPage from "./_components/blogs-page";
 
 const page = async () => {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/blogs?limit=100`);
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL || "http://119.59.102.217:3000"}/blogs?limit=100`,
+  );
   const blogs: { data: Blog[] } = await res.json();
   return (
     <div className="pt-0">
--- web-game-laos/app/page.tsx
+++ web-game-laos/app/page.tsx
@@ -2,7 +2,9 @@ import HomePage from "./_components/home-page";
 import { BannerType } from "@/types/banner.type";
 
 export default async function Home() {
-  const res = await fetch(`${process.env.API_URL || ""}/banner`);
+  const res = await fetch(
+    `${process.env.API_URL || "http://119.59.102.217:3000"}/banner`,
+  );
   const data = (await res.json()) as BannerType[];
   return <HomePage banner={data || []} />;
 }
